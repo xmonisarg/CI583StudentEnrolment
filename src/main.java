4b52ad355108ef7e625417ce601bcb726d2b147c
xmonisarg <monicasargentp@gmail.com>
2026-04-29 15:31:05 +0100

    Update file paths for student enrolment data and add CSV data file

--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {  
     // Main to read and create the CSV file parser.
   public static void main(String[] args) throws IOException, InterruptedException {
-    Scanner scanner = new Scanner(new File("src/StudentEnrolmentData.txt"));
+    Scanner scanner = new Scanner(new File("src/StudentEnrolmentData.csv"));
     List<StudentEnrol> processes = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
